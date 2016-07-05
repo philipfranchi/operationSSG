9de5a3d562c0e5791a99ef0459695acaa0f11c26
emensch <eli.mensch@gmail.com>
2016-07-04 22:23:37 -0400

    Add link to GameView from Home

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router';
 import cn from 'classnames';
 
 export default class Home extends React.Component {
     render() {
         return (
             <div className={cn('home')}>
-                Hello World!
+                <p> Hello World! </p>
+                <Link to='/game'>GAME TIME</Link>
             </div>
         )
     }
