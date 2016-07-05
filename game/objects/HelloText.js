469ba137e56753848cb1240bda2891d0630f20a0
emensch <eli.mensch@gmail.com>
2016-07-04 22:53:55 -0400

    Test phaser interactivity

--- game/objects/HelloText.js
+++ game/objects/HelloText.js
@@ -3,6 +3,28 @@ import Phaser from 'phaser';
 export default class HelloText extends Phaser.Text {
     constructor(game, x, y) {
         super(game, x, y, 'Hello World', { font: "45px Arial", fill: "#ff0044", align: "center" })
-        this.game.stage.addChild(this);
+
+        this.inputEnabled = true;
+
+        this.events.onInputOver.add(this.over, this);
+        this.events.onInputOut.add(this.out, this);
+
+        this.game.stage.addChild(this)
+
+        this.rotating = false;
+    }
+
+    over(item) {
+        this.rotating = true;
+    }
+
+    out(item) {
+        this.rotating = false;
+    }
+
+    update() {
+        if(this.rotating) {
+            this.angle += 1
+        }
     }
 }
